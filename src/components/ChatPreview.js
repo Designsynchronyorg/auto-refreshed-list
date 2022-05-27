93cd8d65e1e3ef5c8bd49b94eab6e3bb7977d2ff
timmichael738 <timmichael738@gmail.com>
2022-05-27 02:50:09 +0100

    --- Auto-Git Commit ---

---
+++ src/components/ChatPreview.js
@@ -0,0 +1,39 @@
+
+import { Component } from 'react';
+import Avatar from '../features/Avatar';
+
+export default class ChatPreview extends Component {
+    constructor(props) {
+        super(props);
+
+        // bind this
+        this.loadChat = this.loadChat.bind(this);
+    }
+
+    loadChat = () => {
+        // pass user id to parent
+        this.props.loadChat(this.props.user);
+    }
+
+    render() {
+        return (
+            <>
+                <div onClick={this.loadChat} className={(
+                    // if chat has not been read
+                    this.props.user.message.is_read === '0'
+                        &&
+                        // and current user is the receiver
+                        (this.props.user.message.receiver.id === JSON.parse(sessionStorage.getItem('user')).id)
+                        ? 'bg-gray-300 dark:bg-gray-900 hover:gray-text white-gray-bg-hover light-gray-border'
+                        : 'white-gray-bg hover:gray-text white-gray-bg-hover light-gray-border'
+                ) + " cursor-pointer  p-2 2xs:p-4  shadow-md "
+                }>
+                    <Avatar pic={this.props.user.image} contClass="flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-4 sm:items-center" name={this.props.user.name} />
+                    <div className="w-[80px] 3xs:w-[150px] 2xs:w-[250px] xs:w-[350px] sm:w-[520px]">
+                        <p className="my-4 md:mt-8 truncate font-normal text-gray-700 dark:text-gray-400">{this.props.user.message.message}</p>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import Component from './components/Component'
+import ChatList from './components/ChatList'
+import ChatListContent from './components/ChatListContent'
+import ChatListHeader from './components/ChatListHeader'
+import ChatPreview from './components/ChatPreview';
 
 export {
-    Component
+    ChatList,
+    ChatListContent,
+    ChatListHeader,
+    ChatPreview,
 };
